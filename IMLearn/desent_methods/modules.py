Fixed L1, L2 modules (jacobian)

IMLearn/desent_methods/modules.py
@@ -33,7 +33,7 @@ class L2(BaseModule):
         output: ndarray of shape (1,)
             Value of function at point self.weights
         """
-        return np.linalg.norm(self.weights).reshape(1, ) ** 2
+        return np.linalg.norm(self.weights_).reshape(1, ) ** 2
 
     def compute_jacobian(self, **kwargs) -> np.ndarray:
         """
@@ -49,7 +49,8 @@ class L2(BaseModule):
         output: ndarray of shape (n_in,)
             L2 derivative with respect to self.weights at point self.weights
         """
-        return np.gradient(self.weights)
+        return self.weights_
+        # return np.gradient(self.weights)
 
 
 class L1(BaseModule):
@@ -94,7 +95,8 @@ class L1(BaseModule):
         output: ndarray of shape (n_in,)
             L1 derivative with respect to self.weights at point self.weights
         """
-        return np.gradient(self.weights)
+        return np.sign(self.weights_)
+        # return np.gradient(self.weights)
 
 
 class LogisticModule(BaseModule):